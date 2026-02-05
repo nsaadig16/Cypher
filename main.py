6683e9005961fc95fd87f11f398dbc41477080ac
nsaadig16 <nsaadig16@gmail.com>
2026-02-05 21:28:25 +0100

    feat: Add leaderbord

--- main.py
+++ main.py
@@ -3,21 +3,22 @@ import requests
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
-from utils import get_color_from_image, get_rank_icon
+from utils import get_color_from_image, get_rank_icon, get_name_tag, get_rank_int_value, rgb
 
 load_dotenv()
-token = os.getenv("TOKEN","")
-api_key = os.getenv("API_KEY","")
-
-headers = {
-    "Authorization" : api_key,
+TOKEN = os.getenv("TOKEN","")
+API_KEY = os.getenv("API_KEY","")
+HEADERS = {
+    "Authorization" : API_KEY,
     "Content-Type": "application/json",
     "Accept" : "*/*"
 }
+BLUE = (90, 200, 250)
+RED = (255, 70, 85)
+REGION = "eu"
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
@@ -25,25 +26,21 @@ async def on_ready():
     print("Cypher's in!")
 
 @bot.command()
-async def test(ctx, *args):
-    await ctx.send(f"You said {' '.join(args)}")
-
-@bot.command()
 async def is_up(ctx):
-    region = "eu"
-    response = requests.get(f"https://api.henrikdev.xyz/valorant/v1/status/{region}", headers=headers).json()
+    response = requests.get(f"https://api.henrikdev.xyz/valorant/v1/status/{REGION}", headers=HEADERS).json()
     is_up = response["status"]
 
+    # Check status code
     if is_up == 200:
         await ctx.send("The server for the Europe region is up!")
     else:
         await ctx.send("The server for the Europe region is down!")
 
-@bot.command()
-async def player(ctx, *nametag):
-    name_and_tag = ''.join(nametag)
-    name, tag = name_and_tag.strip().split('#')
-    response = requests.get(f"https://api.henrikdev.xyz/valorant/v1/account/{name}/{tag}", headers=headers).json()
+@bot.command(name="player")
+async def get_player(ctx, *nametag):
+    # Get data
+    name, tag = get_name_tag(nametag)
+    response = requests.get(f"https://api.henrikdev.xyz/valorant/v1/account/{name}/{tag}", headers=HEADERS).json()
     account_level = response["data"]["account_level"]
     card = response["data"]["card"]
     image = card["small"]
@@ -51,6 +48,7 @@ async def player(ctx, *nametag):
     rank = get_rank_from_nametag(name,tag)
     rank_icon = get_rank_icon(get_rank_int_value(rank))
 
+    # Build embed
     embed = discord.Embed(
         description=f"## {name}#{tag}\n ### Level {account_level}",
         color=color
@@ -59,24 +57,48 @@ async def player(ctx, *nametag):
     embed.set_thumbnail(url=rank_icon)
     embed.set_image(url=image)
 
+    # Send embed
     await ctx.send(embed=embed)
 
+@bot.command(name='last_match')
+async def get_last_match_leaderboard(ctx, *nametag):
+    # Get data
+    name, tag = get_name_tag(nametag)
+    response = requests.get(url=f"https://api.henrikdev.xyz/valorant/v3/matches/eu/{name}/{tag}",headers=HEADERS).json()
+    last_match = response["data"][0]
+    rounds = last_match["metadata"]["rounds_played"]
+    players = last_match["players"]["all_players"]
+    sorted_players = sorted(players, key=lambda x : x["stats"]["score"]//rounds,reverse=True)
+
+    embeds = []
+    for player in sorted_players:
+        player_name = f"{player["name"]}#{player["tag"]}"
+        score = player["stats"]["score"] // rounds
+        k, d, a = player["stats"]["kills"], player["stats"]["deaths"], player["stats"]["assists"]
+        color = rgb(*RED) if player["team"] == "Red" else rgb(*BLUE) 
+
+        embed =discord.Embed(
+                color=color,
+                description=f"### {player_name}",
+            )
+        embed.add_field(
+            name="```Score```",
+            value=f"{score}"
+        )
+        embed.add_field(
+            name="```K/D/A```",
+            value=f"{k}/{d}/{a}",
+        )
+        embed.set_thumbnail(
+            url=player["assets"]["agent"]["small"]
+        )
+        embed.set_image(url="https://i.sstatic.net/Fzh0w.png")
+        embeds.append(embed)
+    await ctx.send(embeds=embeds)
+
 def get_rank_from_nametag(name, tag):
-    response = requests.get(f"https://api.henrikdev.xyz/valorant/v3/mmr/eu/pc/{name}/{tag}", headers=headers).json()
+    response = requests.get(f"https://api.henrikdev.xyz/valorant/v3/mmr/eu/pc/{name}/{tag}", headers=HEADERS).json()
     return response["data"]["current"]["tier"]["name"].upper()
 
-def get_rank_int_value(rank : str):
-    print(rank)
-    vals = {"IRON" : 2, "BRONZE" : 5, "SILVER" : 8,
-            "GOLD" : 11, "PLATINUM" : 14, "DIAMOND" : 17, "ASCENDANT" : 20, "IMMORTAL" : 23,
-    }
-    tier = rank.split()[0]
-    print(f"tier {tier}")
-    if tier == "UNRATED":
-        return 0
-    elif tier == "RADIANT":
-        return 27
-    else:
-        return vals[tier] + int(rank.split()[1])
-
-bot.run(token=token)
+if __name__ == "__main__": 
+    bot.run(token=TOKEN)
--- utils.py
+++ utils.py
@@ -1,14 +1,41 @@
 import requests
 from colorthief import ColorThief
 from io import BytesIO
+from typing import Tuple
 
 def get_color_from_image(url : str):
     response = requests.get(url)
     img = BytesIO(response.content)
     color_thief = ColorThief(img)
     r, g, b = color_thief.get_color(quality=1)
-    return (r << 16) + (g << 8) + b
+    return rgb(r,g,b)
 
-def get_rank_icon( num : int):
+def get_rank_icon(num : int):
     response = requests.get("https://valorant-api.com/v1/competitivetiers").json()
     return response["data"][-1]["tiers"][num]["smallIcon"]
+
+def get_name_tag(nametag: Tuple[str, ...]):
+    name_and_tag = "".join(nametag)
+    return name_and_tag.strip().split("#")
+
+def get_rank_int_value(rank: str):
+    vals = {
+        "IRON": 2,
+        "BRONZE": 5,
+        "SILVER": 8,
+        "GOLD": 11,
+        "PLATINUM": 14,
+        "DIAMOND": 17,
+        "ASCENDANT": 20,
+        "IMMORTAL": 23,
+    }
+    tier = rank.split()[0]
+    if tier == "UNRATED":
+        return 0
+    elif tier == "RADIANT":
+        return 27
+    else:
+        return vals[tier] + int(rank.split()[1])
+
+def rgb(r, g, b):
+    return (r << 16) + (g << 8) + b
