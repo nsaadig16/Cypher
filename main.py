85c2fe3d1315597d76bdde95a87e689cca7ca0b9
nsaadig16 <nsaadig16@gmail.com>
2026-02-06 15:55:02 +0100

    fix: Error message pops up when name not found

--- main.py
+++ main.py
@@ -41,11 +41,18 @@ async def get_player(ctx, *nametag):
     # Get data
     name, tag = get_name_tag(nametag)
     response = requests.get(f"https://api.henrikdev.xyz/valorant/v1/account/{name}/{tag}", headers=HEADERS).json()
+    if check_request(response):
+        await ctx.send(check_request(response))
+        return
+
     account_level = response["data"]["account_level"]
     card = response["data"]["card"]
     image = card["small"]
     color = get_color_from_image(card["small"])
     rank = get_rank_from_nametag(name,tag)
+    if rank.startswith("Error"):
+        await ctx.send(rank)
+        return
     rank_icon = get_rank_icon(get_rank_int_value(rank))
 
     # Build embed
@@ -60,11 +67,15 @@ async def get_player(ctx, *nametag):
     # Send embed
     await ctx.send(embed=embed)
 
-@bot.command(name='last_match')
+@bot.command(name='lastmatch')
 async def get_last_match_leaderboard(ctx, *nametag):
     # Get data
     name, tag = get_name_tag(nametag)
     response = requests.get(url=f"https://api.henrikdev.xyz/valorant/v3/matches/eu/{name}/{tag}",headers=HEADERS).json()
+    if check_request(response):
+        await ctx.send(check_request(response))
+        return
+
     last_match = response["data"][0]
     rounds = last_match["metadata"]["rounds_played"]
     players = last_match["players"]["all_players"]
@@ -98,7 +109,16 @@ async def get_last_match_leaderboard(ctx, *nametag):
 
 def get_rank_from_nametag(name, tag):
     response = requests.get(f"https://api.henrikdev.xyz/valorant/v3/mmr/eu/pc/{name}/{tag}", headers=HEADERS).json()
+    if response.get("errors",False):
+        message = response["errors"][0]["message"]
+        return "Error: " + message
     return response["data"]["current"]["tier"]["name"].upper()
+
+def check_request(response):
+    if response.get("errors",False):
+        message = response["errors"][0]["message"]
+        return f"Error: {message}"
+    return None
 
 if __name__ == "__main__": 
     bot.run(token=TOKEN)
